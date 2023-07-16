7095543a589562025ded7b75be71ee6892d4d0ac
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-07-16 07:37:50 +0000

    20230711_アプリケーションを完成させようのバリデーションの実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book created successfully "
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books=Book.all
+      render :index
     end
   end
 
@@ -27,9 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        flash[:notice] = "Book updated successfully "
+        redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
